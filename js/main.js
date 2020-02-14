f2a89afafb3e3ef26766bab7a29e808fbbe6f6a3
idanyviat <idanyviat@gmail.com>
2020-02-14 09:10:25 +0300

    v2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 var PHOTOS_AMUNT = 25;
 var COMMENT_MESSAGE = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-var photoTemplate = document.querySelector('#picture');
+var photoTemplate = document.querySelector('#picture').content.querySelector('.picture');
 var photoList = document.querySelector('.pictures');
 
 var getRandomArbitrary = function (min, max) {
@@ -40,7 +40,7 @@ var generateComments = function () {
 
 var renderPhoto = function (photoData) {
   var generateImage = photoTemplate.cloneNode(true);
-  generateImage.querySelector('src').textContent = photoData.url;
+  generateImage.querySelector('.picture').src = photoData.url;
   generateImage.querySelector('.picture__likes').textContent = photoData.likes;
   generateImage.querySelector('.picture__comments').textContent = photoData.comments;
   return generateImage;
